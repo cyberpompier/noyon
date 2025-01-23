b53fbef665f4feea565eb972cbacd199be542e97
cyberpompier <sebastien.dupressoir@icloud.com>
2025-01-23 14:42:41 +0100

    Initial commit from your app

--- src/VehiclesPage.jsx
+++ src/VehiclesPage.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './VehiclesPage.css';
 
 function VehiclesPage({ vehicles, onClose }) {
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+
+  const handleThumbnailClick = (photo) => {
+    setSelectedPhoto(photo);
+  };
+
+  const handleClosePopup = () => {
+    setSelectedPhoto(null);
+  };
+
   return (
     <div className="vehicles-page">
       <div className="vehicles-header">
@@ -11,7 +21,12 @@ function VehiclesPage({ vehicles, onClose }) {
       <ul>
         {vehicles.map((vehicle, index) => (
           <li key={index} className="vehicle-card">
-            <img src={vehicle.photo || 'default-image-url.jpg'} alt={vehicle.denomination} className="vehicle-photo" />
+            <img
+              src={vehicle.photo || 'default-image-url.jpg'}
+              alt={vehicle.denomination}
+              className="vehicle-photo"
+              onClick={() => handleThumbnailClick(vehicle.photo)}
+            />
             <div className="vehicle-info">
               <h3>{vehicle.denomination}</h3>
               <p>Immatriculation: {vehicle.immatriculation}</p>
@@ -24,6 +39,14 @@ function VehiclesPage({ vehicles, onClose }) {
           </li>
         ))}
       </ul>
+      {selectedPhoto && (
+        <div className="popup" onClick={handleClosePopup}>
+          <div className="popup-content" onClick={(e) => e.stopPropagation()}>
+            <span className="popup-close" onClick={handleClosePopup}>✖</span>
+            <img src={selectedPhoto} alt="Selected Vehicle" className="popup-image" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
